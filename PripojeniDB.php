edf7bb906f61756dd8456598046951dcec2a3f8b
MilanStud <116145137+MilanStud@users.noreply.github.com>
2023-01-30 13:42:32 +0100

    Úprava připojení

--- PripojeniDB.php
+++ PripojeniDB.php
@@ -4,10 +4,4 @@ $server = "localhost";
 $uzivatel = "anketa";
 $heslo = "123";
 $databaze = "anketa";
-
-// $server = "uvdb58.active24.cz";
-// $uzivatel = "volutasper";
-// $heslo = "nmXmCvmJ";
-// $databaze = "volutasper";
-
 ?>
